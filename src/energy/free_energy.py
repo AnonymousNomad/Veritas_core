2284e22a206269b624c0e6acd2cd625480299f1f
FerrellSyntheticIntelligence <Neuro_nomad@outlook.com>
2026-05-28 21:45:05 +0000

    Fix: AtomicCore implementation and module pathing

--- src/energy/free_energy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-class FreeEnergyCalculator:
-    def __init__(self, alpha=0.85):
-        self.alpha = alpha
-        self.surprise = 0.0
-    def update(self, log_likelihood):
-        self.surprise = (self.alpha * self.surprise) + ((1 - self.alpha) * -log_likelihood)
-        return self.surprise
